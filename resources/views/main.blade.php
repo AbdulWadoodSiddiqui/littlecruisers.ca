ee842bdbcb5067a786f392fdc4e46cbde93c646f
Abrar Amjad <ranabobi30@gmail.com>
2024-11-21 00:21:59 +0500

    add js

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -396,6 +396,15 @@
     <script src="assets/vendors/countdown/countdown.min.js"></script>
     <script src="assets/vendors/jquery-circleType/jquery.circleType.js"></script>
     <script src="assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16486838963"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-16486838963');
+    </script>
     <!-- template js -->
     <script src="assets/js/kidearn.js"></script>
 </body>
